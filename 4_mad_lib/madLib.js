7fd0f365180d2563ba0a5bb380e68f062241ea3f
fondlykip <liam.j.campbell@hotmail.co.uk>
2019-06-07 17:24:14 +0100

    added form functionalituy to madlib code

--- 4_mad_lib/madLib.js
+++ 4_mad_lib/madLib.js
@@ -1,15 +1,29 @@
 
 //prompt for input of values
-var nounIn = prompt("enter a noun")
-var verbIn = prompt("enter a verb")
-var adjIn = prompt("enter an adjective")
-var advIn = prompt("enter an adverb")
+//var nounIn = prompt("enter a noun")
+//var verbIn = prompt("enter a verb")
+//var adjIn = prompt("enter an adjective")
+//var advIn = prompt("enter an adverb")
 
 //function takes inputed values, returns madlib
 function genOut(n,v,adj,adv){
     //template literal syntax with backticks `this is a templae ${var}`
-    return(`Do you ${v} your ${adj} ${n} ${adv}? That's hilarious!`)
+    return(`Do you ${v} your ${adj} ${n} ${adv}?<br> That's hilarious!`)
 }
 
+//handle form
+function formHandleFunc(){
+
+    //pass form valuse to variables
+    var n = document.getElementById("form-noun").value;
+    var v = document.getElementById("form-verb").value;
+    var adj = document.getElementById("form-adjective").value;
+    var adv = document.getElementById("form-adverb").value;
+
+    //output of genOut() func added to madLibOut <p> element 
+    document.getElementById("madLibOut").innerHTML = genOut(n,v,adj,adv)
+}
+
+
 //output function result
-document.write(genOut(nounIn,verbIn,adjIn,advIn))
+//document.write(genOut(nounIn,verbIn,adjIn,advIn))
