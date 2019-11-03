243c3ef07304fff0ce9f387a5b1d41a948876ce7
fondlykip <liam.j.campbell@hotmail.co.uk>
2019-11-03 15:43:10 +0000

    added changes from ex 30

--- 30_multiplication_table/multiplicationTable.js
+++ 30_multiplication_table/multiplicationTable.js
@@ -33,9 +33,3 @@ function generateTable(n){
     document.getElementById("outputTable").innerHTML = outputHTML;
     //console.log(outputHTML);
 };
-
-function isPrime(num) {
-    for(var i = 2; i < num; i++)
-      if(num % i === 0) return false;
-    return num > 1;
-  }
